4a52cd8490d22d4948b11bcf78777f95d0aec2fa
geoffrey0pv <geoffreypv00@gmail.com>
2024-03-28 14:55:57 -0500

    add other classes about chain pattern

---
+++ Pattern Design Implemetation/src/model/ChainResponsabilityPattern/FileLogger.java
@@ -0,0 +1,11 @@
+package model.ChainResponsabilityPattern;
+
+public class FileLogger extends AbstractLogger{
+    public FileLogger(int level){
+        this.level = level;
+    }
+    @Override
+    protected void write(String message) {
+        System.out.println("File::Logger: " + message);
+    }
+}
